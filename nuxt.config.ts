1323f7f363f03bf6003050ac83784b40f7637633
Alexey Kozhevnikov <alexey.kozhevnikov@gmail.com>
2026-05-11 10:39:31 +0300

    Настройк для сессий

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', 'nuxt-auth-utils'],
+  runtimeConfig: {
+    session: {
+      maxAge: 60 * 60 * 24 * 7, // 1 week
+      name: 'tamara-session',
+      password: process.env.NUXT_SESSION_PASSWORD || ''
+    }
+  },
   css: ['~/assets/css/main.css'],
   routeRules : {
     '/welcome': {
